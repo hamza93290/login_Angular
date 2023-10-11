458aa203fb87c1e6c1bb547065569e0780df68e4
Hamza HAMZAOUI <HHA24085@open-groupe.com>
2023-10-11 11:02:24 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
